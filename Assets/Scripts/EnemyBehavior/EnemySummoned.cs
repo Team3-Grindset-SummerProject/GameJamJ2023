bd82427eeea0cabaf0c04184ad1f9ec17b823e72
AlmityTuhm <driggsthomas@gmail.com>
2023-01-22 16:32:30 -0700

    Summoning Works

--- Assets/Scripts/EnemyBehavior/EnemySummoned.cs
+++ Assets/Scripts/EnemyBehavior/EnemySummoned.cs
@@ -6,6 +6,11 @@ using UnityEngine.AI;
 public class EnemySummoned : MonoBehaviour
 {
     [SerializeField] private GameObject enemy = null;
+    [SerializeField] private GameObject enemySprite = null;
+    [SerializeField] private GameObject player = null;
+
+    private float xPos = 0.0f;
+    private float yPos = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +20,20 @@ public class EnemySummoned : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.other.gameObject.CompareTag("Player"))
+        xPos = Mathf.Abs(player.transform.position.x - this.transform.position.x);
+        yPos = Mathf.Abs(player.transform.position.y - this.transform.position.y);
+        if(xPos <= 0.2 && yPos <= 0.2)
         {
-            enemy.GetComponent<BigBadBehavior>().enabled = !enemy.GetComponent<BigBadBehavior>().enabled;
-            enemy.GetComponent<NavMeshAgent>().enabled = !enemy.GetComponent<NavMeshAgent>().enabled;
-
+            SummonHim();
         }
     }
+
+    private void SummonHim()
+    {
+        enemySprite.SetActive(true);
+        enemy.GetComponent<BigBadBehavior>().enabled = !enemy.GetComponent<BigBadBehavior>().enabled;
+        enemy.GetComponent<NavMeshAgent>().enabled = !enemy.GetComponent<NavMeshAgent>().enabled;
+        Destroy(this);
+    }
+
 }

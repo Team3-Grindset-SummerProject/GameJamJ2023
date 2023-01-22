c1a2c554025875d73d23d7fdf0ea4773b2be0e2b
Phoenixxi <rowan809xi@gmail.com>
2023-01-22 14:32:26 -0700

    Pre final scene

---
+++ Assets/Scripts/Buttons/RemoveInventory.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class RemoveInventory : MonoBehaviour
+{
+    public TextMeshProUGUI AcidNumber;
+    public TextMeshProUGUI TimeNumber;
+    public TextMeshProUGUI BlackHoleNumber;
+
+     public int AcidNum = 0;
+    public int TimeNum = 0;
+    public int BlackHoleNum = 0;
+
+
+    public void AcidTrapPlace()
+    {
+        AcidNum--;
+        AcidNumber.text = AcidNum.ToString();
+    }
+
+    public void TimeTrapPlace()
+    {
+        TimeNum--;
+        TimeNumber.text = TimeNum.ToString();
+
+    }
+
+    public void BLackHoleTrapPlace()
+    {
+        BlackHoleNum--;
+        BlackHoleNumber.text = BlackHoleNum.ToString();
+    }
+}
